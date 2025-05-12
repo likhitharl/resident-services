fb34797cb75a9a3fcf4802c0232a194004760b1d
kameshsr <kameshsr1338@gmail.com>
2025-05-12 18:25:10 +0530

    MOSIP-41336 removed un-necessary comments
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/interceptor/ResidentEntityInterceptor.java
+++ resident/resident-service/src/main/java/io/mosip/resident/interceptor/ResidentEntityInterceptor.java
@@ -96,7 +96,7 @@ public class ResidentEntityInterceptor implements Interceptor, Serializable {
 	public boolean onFlushDirty(Object entity, Object id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
 		if (entity instanceof ResidentTransactionEntity) {
 			List<String> propertyNamesList = Arrays.asList(propertyNames);
-			encryptDataOnSave(id, currentState, propertyNamesList, types, (ResidentTransactionEntity) entity); // ✅ Use currentState, not previousState
+			encryptDataOnSave(id, currentState, propertyNamesList, types, (ResidentTransactionEntity) entity);
 		}
 		return Interceptor.super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
 	}
