447d563b37fc5f2574a3995a04224eb7f9e88d20
SradhaMohanty5899 <mohantysradha10@gmail.com>
2025-05-19 12:53:51 +0530

    MOSIP-40258
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/resident/testscripts/AddIdentity.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/resident/testscripts/AddIdentity.java
@@ -38,6 +38,7 @@ import io.mosip.testrig.apirig.utils.KernelAuthentication;
 import io.mosip.testrig.apirig.utils.OutputValidationUtil;
 import io.mosip.testrig.apirig.utils.ReportUtil;
 import io.mosip.testrig.apirig.utils.RestClient;
+import io.mosip.testrig.apirig.utils.SecurityXSSException;
 import io.restassured.response.Response;
 
 public class AddIdentity extends ResidentUtil implements ITest {
@@ -84,7 +85,7 @@ public class AddIdentity extends ResidentUtil implements ITest {
 	 * @throws AdminTestException
 	 */
 	@Test(dataProvider = "testcaselist")
-	public void test(TestCaseDTO testCaseDTO) throws AuthenticationTestException, AdminTestException {
+	public void test(TestCaseDTO testCaseDTO) throws AuthenticationTestException, AdminTestException, SecurityXSSException {
 		testCaseName = testCaseDTO.getTestCaseName();
 		testCaseName = ResidentUtil.isTestCaseValidForExecution(testCaseDTO);
 		if (HealthChecker.signalTerminateExecution) {
@@ -170,7 +171,7 @@ public class AddIdentity extends ResidentUtil implements ITest {
 		try {
 			logger.info(
 					"waiting for " + properties.getProperty("Delaytime") + " mili secs after UIN Generation In IDREPO");
-			Thread.sleep(Long.parseLong(properties.getProperty("Delaytime")));
+			/* Thread.sleep(Long.parseLong(properties.getProperty("Delaytime"))); */
 		} catch (Exception e) {
 			logger.error("Exception : " + e.getMessage());
 			Thread.currentThread().interrupt();
