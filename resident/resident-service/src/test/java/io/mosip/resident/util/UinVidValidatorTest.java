21ee2751989db49f4925459708a0bdbee87a5115
kameshsr <kameshsr1338@gmail.com>
2025-07-10 12:55:15 +0530

    MOSIP-39072 corrected as per review comments
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/test/java/io/mosip/resident/util/UinVidValidatorTest.java
+++ resident/resident-service/src/test/java/io/mosip/resident/util/UinVidValidatorTest.java
@@ -24,7 +24,7 @@ import static org.mockito.Mockito.when;
 public class UinVidValidatorTest {
 
     @InjectMocks
-    private UinVidValidator uinVidValidator = new UinVidValidator();
+    private UinVidValidator uinVidValidator;
 
     @Mock
     private UinValidator<String> uinValidator;
